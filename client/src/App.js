616a939eb4eb02577b35d4f5cf2c560496fc8cca
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-11 00:28:53 -0800

    Added Header component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/scss/bootstrap.scss";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div class="container py-2">
+      <Header/>
+      </div>
     </>
   );
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function Header() {
   return(
-
+    <Jumbotron>
+      <h1 class="text-center"> (React) Google Books Search </h1>
+      <p class="text-center"> Search for and Save Books of Interest </p>
+    </Jumbotron>
   )
 };
 
