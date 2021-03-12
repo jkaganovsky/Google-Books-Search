5aec15c2b6721b1b4a6bac648b18d7e44ec4cbfa
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-11 21:41:55 -0800

    Added connections to mongoDB, api routes, and express from client folder.

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Connection to mongoDB
+require("./config/connect");
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connecting with Express
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Connecting with Routes
+app.use(require("./routes/api"));
 
 // Send every request to the React app
 // Define any API routes before this runs
