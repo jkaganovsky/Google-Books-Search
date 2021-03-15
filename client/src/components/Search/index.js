721686b6d3b0d863bd2bdaa0e1c2c2f81e239ddc
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-15 00:04:42 -0700

    Moved button to flex-end.

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import API from "../../utils/api";
-import { Card, Form, Button } from "react-bootstrap";
+import { Card, Form, Col, Button } from "react-bootstrap";
 
 
 function SearchForm({ setBooks }) {
@@ -23,11 +23,13 @@ function SearchForm({ setBooks }) {
         <Form.Group>
           <Form.Label>Book Search</Form.Label>
           <Form.Control ref={bookInput} type="search" placeholder="Enter book title"/>
-        </Form.Group>
+        </Form.Group >
 
-        <Button className="align-items-right" variant="primary" type="submit" onClick={handleSubmit}>
-          Search
-        </Button>
+        <Col style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Search
+          </Button>
+        </Col>
       </Form>
     </Card>
   )
