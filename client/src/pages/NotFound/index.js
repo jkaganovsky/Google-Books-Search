9d821b06a61a2afee48669e162cbb6fe75dc88d3
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-14 22:05:01 -0700

    Added a not found page.

---
+++ client/src/pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return(
+    <div className="p-2 text-center">
+
+      <h5>PAGE NOT FOUND</h5>
+
+    </div>
+  )
+}
+
+export default NotFound;
