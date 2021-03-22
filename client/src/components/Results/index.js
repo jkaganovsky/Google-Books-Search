35adff5b00ab8093edd85893b0bd6bbb8f06d059
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-21 22:15:44 -0700

    Changed view button to href.

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Card, Media, Row, Col } from "react-bootstrap";
+import SaveBtn from "../SaveBtn";
 
 function Results({ books }) {
   return (
@@ -15,13 +16,13 @@ function Results({ books }) {
               <Card className="p-2" border="info" key={book.id}>
                 <Card.Body>
                   <Row className="p-3">
+                    <Col sm={4} style={{display: 'flex', justifyContent: 'flex-end'}}>
+                      <Button href={book.volumeInfo.infoLink}>View</Button>
+                      <SaveBtn book={book.id}/>
+                    </Col>
                     <Col sm={8}>
                       <h6>{book.volumeInfo.title}</h6>
-                      <h7>Written By: {book.volumeInfo.authors}</h7>
-                    </Col>
-                    <Col sm={4}>
-                      <Button className="m-1" type="view">View</Button>
-                      <Button className="m-1" type="save">Save</Button>
+                      <h6>Written By: {book.volumeInfo.authors}</h6>
                     </Col>
                   </Row>
 
@@ -31,7 +32,7 @@ function Results({ books }) {
                         width={64}
                         height={64}
                         className="mr-3"
-                        src={book.volumeInfo.imageLinks.thumbnail}
+                        src={book.volumeInfo.imageLinks.smallThumbnail}
                         alt={book.volumeInfo.title}
                       />
                       <Media.Body>
