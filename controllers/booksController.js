17433f1ad941869a9b8012843b4bf2d0d4f1b5a7
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-28 23:20:40 -0700

    Added bookControllers.

---
+++ controllers/booksController.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: (req, res) => {
+    db.Book
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: (req, res) => {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+  remove: (req, res) => {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
